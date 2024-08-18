dea2b4cdad061a3abd4ab946fd2c3e77d43f3b52
Caloy255 <kaloygarcia8@gmail.com>
2024-08-18 19:07:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// TEXT AREA
+document.addEventListener("DOMContentLoaded", function () {
+    let textarea = document.getElementById("message");
+    textarea.addEventListener("input", function () {
+      this.style.height = "auto";
+      this.style.height = this.scrollHeight + "px";
+    });
+  });
+
+  // TOGGLE MENU
+  const burgerIcon = document.getElementById("burger-icon");
+  const navLinks = document.getElementById("nav-links");
+  
+  function toggleNavLinks() {
+    if (window.innerWidth <= 834) {
+      if (navLinks.style.display !== "block") {
+        navLinks.style.display = "block";
+      } else {
+        navLinks.style.display = "none";
+      }
+    }
+  }
+  
+  burgerIcon.addEventListener("click", function () {
+    toggleNavLinks();
+  });
+  
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 834) {
+      navLinks.style.display = "block";
+    } else {
+      navLinks.style.display = "none";
+    }
+  });
+  
+  // Set initial display style of nav links based on window width
+  if (window.innerWidth <= 834) {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "block";
+  }
+
+  
+  
